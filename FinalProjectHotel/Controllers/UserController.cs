e58397f68e3673c48c1119786642cbd89f57f027
AlvaroRetana <44202909+AlvaroRetana@users.noreply.github.com>
2019-05-31 16:45:50 -0600

    Commit Login 2

--- FinalProjectHotel/Controllers/UserController.cs
+++ FinalProjectHotel/Controllers/UserController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace FinalProjectHotel.Controllers
 {
     public class UserController : Controller
     {
+
+        SqlConnection conexion = new SqlConnection("Data Source=serviciosweb.database.windows.net;Initial Catalog=Vuelos;Persist Security Info=True;User ID=vuelosadmin;Password=12458+25Ddfsd$3Dsx+"); //catalog= bd_demo
+
         // GET: User
         public ActionResult Login()
         {
             return View();
         }
+
     }
 }
---
+++ FinalProjectHotel/Scripts/jquery-logic/login-jquery.js
@@ -0,0 +1,3 @@
+﻿var Login = function () {
+    var data =$("")
+}
