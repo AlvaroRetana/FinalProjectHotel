5c6a346f28587fe83085337e96d11f5a403e2f9b
AlvaroRetana <44202909+AlvaroRetana@users.noreply.github.com>
2019-05-31 17:17:37 -0600

    commit Entity Framework

--- FinalProjectHotel/Controllers/UserController.cs
+++ FinalProjectHotel/Controllers/UserController.cs
@@ -5,18 +5,28 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data;
 using System.Data.SqlClient;
+using FinalProjectHotel.Models;
 
 namespace FinalProjectHotel.Controllers
 {
     public class UserController : Controller
     {
 
+
         SqlConnection conexion = new SqlConnection("Data Source=serviciosweb.database.windows.net;Initial Catalog=Vuelos;Persist Security Info=True;User ID=vuelosadmin;Password=12458+25Ddfsd$3Dsx+"); //catalog= bd_demo
 
-        // GET: User
+        Entities db = new Entities();
+
         public ActionResult Login()
         {
             return View();
+        }
+
+        public JsonResult SaveData(Usuario_Admin model)
+        {
+            db.Usuario_Admin.Add(model);
+            db.SaveChanges();
+            return Json("Registration Successfull", JsonRequestBehavior.AllowGet);
         }
 
     }
